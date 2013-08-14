3c8dd76ceff33a56ff41a0c94ee0ce6c023bbb81
Erez A. Korn <erakorn@outlook.com>
2013-08-14 17:07:38 +0300

    Added strong name signing

--- csharp/PhoneNumbers/Properties/AssemblyInfo.cs
+++ csharp/PhoneNumbers/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("PhoneNumbers")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Portable library port of libphonenumber")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Erez A. Korn (esemobilesoftware)")]
 [assembly: AssemblyProduct("PhoneNumbers")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
@@ -18,9 +18,6 @@ using System.Runtime.InteropServices;
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("5bf17f39-109f-40b9-94d3-7d245171b3e5")]
 
 // Version information for an assembly consists of the following four values:
 //
